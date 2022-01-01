ebb103f5483b14f3857bdfaecf7d49c5c87d277d
Neha Joshi <nehajoshigkp@gmail.com>
2022-01-01 23:33:57 +0530

    form added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
 
 function App() {
+  const [values, setValues]  = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
   return (
-    <div className="App">
+    <div className="form-container">
+      <form className="register-form">
+        <input
+        value={values.firstName}
+        className="form-field"
+        placeholder="First name"
+        name="firstName"
+        />
+        <input
+        value={values.lastName}
+        className="form-field"
+        placeholder="Last name"
+        name="lastName"
+        />
+        <input
+        value={values.email}
+        className="form-field"
+        placeholder="Email"
+        name="email"
+        />
+        <button className="form=field">
+          Register
+        </button>
+
+      </form>
     
     </div>
   );
